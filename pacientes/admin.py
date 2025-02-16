1a051a7365eeced35625c0e031e472394c1cb767
Nicolas <nicolasimbe2006@gmail.com>
2025-02-16 19:47:15 -0300

    commit projeto

---
+++ pacientes/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Pacientes, Tarefas
+
+admin.site.register(Pacientes)
+admin.site.register(Tarefas)
+# Register your models here.
---
+++ pacientes/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+from django.urls import reverse
+
+class Pacientes(models.Model):
+    queixa_choices = (
+        ("TDAH", "TDAH"),
+        ("D", "Depressão"),
+        ("A", "Ansiedade"),
+        ("TAG", "Transtorno de Ansiedade Generalizada")
+    )
+    
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=255, null = True, blank=True)
+    foto = models.ImageField(upload_to="fotos")
+    pagamento_em_dia = models.BooleanField(default=True)
+    queixa = models.CharField(max_length=4, choices=queixa_choices)
+
+    def __str__(self):
+        return self.nome
+
+    @property
+    def total_consultas(self):
+        return self.consultas_set.count()
+
+class Tarefas(models.Model):
+    frequencia_choices = (
+        ('D', 'Diário'),
+        ('1S', '1 vez por semana'),
+        ('2S', '2 vezes por semana'),
+        ('3S', '3 vezes por semana'),
+        ('N', 'Ao necessitar')
+    )
+    tarefa = models.CharField(max_length=255)
+    instrucoes = models.TextField()
+    frequencia = models.CharField(max_length=2, choices=frequencia_choices, default='D')
+
+    def __str__(self):
+        return self.tarefa
+
+class Consultas(models.Model):
+    humor = models.PositiveIntegerField()
+    registro_geral = models.TextField()
+    video = models.FileField(upload_to="video")
+    tarefas = models.ManyToManyField(Tarefas)
+    paciente = models.ForeignKey(Pacientes, on_delete=models.CASCADE)
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.paciente.nome
+
+    @property
+    def link_publico(self):
+        return f"http://127.0.0.1:8000{reverse('consulta_publica', kwargs={"id": self.id})}"
+
+""" class Visualizacoes(models.Model):
+    consulta = models.ForeignKey(Consultas, on_delete=models.CASCADE)
+    ip = models.GenericIPAddressField()
+
+    @property
+    def views(self):
+        views = Visualizacoes.objects.filter(consulta=self)
+        totais = views.count()
+        unicas = views.values('ip').distinct().count()
+        return f'{totais} - {unicas}' """
